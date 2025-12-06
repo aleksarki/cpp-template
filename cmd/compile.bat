95af8c26c119e11dd61853cd8ba1a4f52d443c32
aleksarki <sashas200@yandex.ru>
2025-12-06 18:10:51 +0500

    initial commit

---
+++ cmd/compile.bat
@@ -0,0 +1,9 @@
+@mkdir ..\build 2>nul
+@cd ..\build
+@cmake ..\ -G "Visual Studio 17 2022" -A x64
+@cmake --build .\ --config Release
+@del ..\bin\*.* /Q
+@robocopy ..\bin\Release ..\bin\ /MOVE
+@echo All files are placed in bin/
+@cd ..\cmd
+@pause
